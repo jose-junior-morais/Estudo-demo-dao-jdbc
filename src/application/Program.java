a6578ca33a4d446926416d1fdcf3563955840cab
Jose Junior <juniormilian6@gmail.com>
2021-12-25 09:36:28 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Departament obj=new Departament(1,"books");
 		
-		System.out.println(obj);
+		Seller seller=new Seller(21,"bob","bog@gnmail",new Date(),3000.00,obj);
+		
+		System.out.println(seller);
 
 	}
 
