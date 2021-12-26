4f6ba4092da6288e868258a1cadd807aae86242d
Jose Junior <juniormilian6@gmail.com>
2021-12-26 09:00:55 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,9 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao= DaoFactory.createrSellerDao();
+		
+		System.out.println("TEST 1== seller findbyId==");
+		
         Seller seller= sellerDao.findById(3);
         
         System.out.println(seller);
